ee38744e6c64b889230994deb6ef4d0dfdcbf79b
Joe Chung <joechung@microsoft.com>
2018-10-09 15:36:56 -0700

    Open editor after creating a Logic App (#55)
    - Fix #34

--- src/commands/logic-app/createLogicApp.ts
+++ src/commands/logic-app/createLogicApp.ts
@@ -3,13 +3,14 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzureTreeDataProvider, IAzureParentNode } from "vscode-azureextensionui";
+import { AzureTreeDataProvider, BaseEditor, IAzureNode, IAzureParentNode } from "vscode-azureextensionui";
 
-export async function createLogicApp(tree: AzureTreeDataProvider, subscription?: IAzureParentNode, resourceGroup?: string): Promise<string> {
+export async function createLogicApp(tree: AzureTreeDataProvider, editor: BaseEditor<IAzureNode>, subscription?: IAzureParentNode, resourceGroup?: string): Promise<string> {
     const node = !subscription
         ? await tree.showNodePicker(AzureTreeDataProvider.subscriptionContextValue) as IAzureParentNode
         : subscription;
 
     const logicAppNode = await node.createChild({ resourceGroup });
+    await editor.showEditor(logicAppNode);
     return logicAppNode.id;
 }
