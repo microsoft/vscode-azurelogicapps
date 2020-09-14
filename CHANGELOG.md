91ae7f23747d138a37b18bf76d238e29e54d82fc
Joe Chung <joechung@microsoft.com>
2020-09-14 14:10:00 -0700

    Update version to 0.2.50 (#172)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to the "vscode-logicapps" extension will be documented in th
 
 ## [Unreleased]
 
+## [0.2.50] - 2020-09-14
+### Changed
+- Update designer version to 1.50907.1.3
+
 ## [0.2.49] - 2020-09-09
 ### Changed
 - Update designer version to 1.50831.1.1
--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 export class Constants {
-    public static DesignerVersion = "1.50831.1.1.200829-0958";
+    public static DesignerVersion = "1.50907.1.3.200909-1227";
 
     public static SubscriptionContextValue = "azureextensionui.azureSubscription";
 
