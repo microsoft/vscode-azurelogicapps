42e7856fb65917a68708fc03fc0f251072dd529a
Joe Chung <joechung@microsoft.com>
2022-12-01 15:14:07 -0500

    Update designer version to 2.21110.1.5

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 export class Constants {
-    public static DesignerVersion = "2.21110.1.4";
+    public static DesignerVersion = "2.21110.1.5";
 
     public static SubscriptionContextValue = "azureextensionui.azureSubscription";
 
